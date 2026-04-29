52f22e8ea09b7d2114b1b69a08928bbfbe3d87ba
Michael Easter <codetojoy@gmail.com>
2026-04-29 11:30:04 -0300

    back-end palindrome

--- app/models/PrimeResult.java
+++ app/models/PrimeResult.java
@@ -3,9 +3,11 @@ package models;
 public class PrimeResult {
     public final int value;
     public final boolean isPrime;
+    public final boolean isPalindrome;
 
-    public PrimeResult(int value, boolean isPrime) {
+    public PrimeResult(int value, boolean isPrime, boolean isPalindrome) {
         this.value = value;
         this.isPrime = isPrime;
+        this.isPalindrome = isPalindrome;
     }
 }
